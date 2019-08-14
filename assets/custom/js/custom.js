cf05df03786ea8c9070d092e7f79ae83aaf9403b
Andy Nguyen <aqn.andrew@gmail.com>
2019-08-14 13:54:39 -0700

    bugs

--- assets/custom/js/custom.js
+++ assets/custom/js/custom.js
@@ -1,55 +1,55 @@
 $(document).ready(function(){
-    
+
 "use strict";
-// wow 
+// wow
  new WOW({
-     mobile: false, 
+     mobile: false,
  }).init();
-    
+
  $(".fables-mega-menu li a").click(function (){
     $(".fables-mega-menu li > div").toggleClass("show-sub");
  });
-    
+
 // 3 items carousel carousel-items-3
- $(".carousel-items-3").owlCarousel({  
+ $(".carousel-items-3").owlCarousel({
      margin:20,
      loop:true,
      navText: ['<span class="fables-iconarrow-left"></span>','<span class="fables-iconarrow-right"></span>'],
      responsive:{
         0:{
-            items:1 
+            items:1
         },
         600:{
-            items:2 
+            items:2
         },
         768:{
-            items:3 
+            items:3
         }
-         
+
     }
- });   
-    
+ });
+
 // 3 items carousel carousel-items-3
- $(".carousel-items-4").owlCarousel({  
+ $(".carousel-items-4").owlCarousel({
      margin:20,
      loop:true,
      navText: ['<span class="fables-iconarrow-left"></span>','<span class="fables-iconarrow-right"></span>'],
      responsive:{
         0:{
-            items:1 
+            items:1
         },
         600:{
-            items:2 
+            items:2
         },
         768:{
-            items:3 
+            items:3
         },
         1000:{
-            items:4 
+            items:4
         }
     }
- });   
-    
+ });
+
 
     // START TOGGLE SEARCH
     $(".open-search").click(function (e){
@@ -58,58 +58,58 @@ $(document).ready(function(){
         $(".search-input").focus();
         $('body').css({"overflow":"hidden"});
     });
-    
+
     $(".close-search").click(function (e){
         e.preventDefault();
         $(".search-section").fadeOut('fast');
         $('body').css({"overflow":"visible"});
     });
-    
+
     $(document).keyup(function(e) {
     if (e.which == 27) {
         $(".search-section").fadeOut('fast');
         $('body').css({"overflow":"visible"});
     }
-    
+
 });
-    
+
 // 3 items carousel carousel-items-6
- $(".carousel-items-6").owlCarousel({  
+ $(".carousel-items-6").owlCarousel({
      margin:20,
      loop:true,
      navText: ['<span class="fables-iconarrow-left"></span>','<span class="fables-iconarrow-right"></span>'],
      responsive:{
         0:{
-            items:2 
+            items:2
         },
         600:{
-            items:3 
+            items:3
         },
         1000:{
-            items:6 
+            items:6
         }
     }
- }); 
-    
- 
-    
+ });
+
+
+
 // teaser slider
  $(".blog-teaser,.default-carousel").owlCarousel({
-     loop:true,  
+     loop:true,
      autoplay:true,
      dots:true,
      nav:true,
      navText: ['<span class="fables-iconarrow-left"></span>','<span class="fables-iconarrow-right"></span>'],
      items:1
  });
-    
+
 // Blog Slider
-    
+
  $(".nav-slider").owlCarousel({
      nav:true,
      loop:true,
      autoplay:true,
-     margin:20, 
+     margin:20,
      navText: ['<span class="fables-iconarrow-left"></span>','<span class="fables-iconarrow-right"></span>'],
      dots:true,
      responsive:{
@@ -124,7 +124,7 @@ $(document).ready(function(){
         }
     }
  });
-    
+
  $("#blog-slider").owlCarousel({
      loop:true,
      margin:20,
@@ -144,8 +144,8 @@ $(document).ready(function(){
         }
     }
  });
-    
- var $input = $("#input-val");   
+
+ var $input = $("#input-val");
  $input.val(1);
     $(".calc-btn").click(function(){
         if($(this).hasClass("plus")) {
@@ -154,32 +154,32 @@ $(document).ready(function(){
             $input.val(parseInt($input.val())-1);
         }
     });
-    
+
 $(".fables-view-btn").click(function() {
      $(this).addClass('active').siblings().removeClass('active');
-    
+
      if($(this).hasClass("fables-list-btn")){
           $(".fables-product-block").removeClass("col-md-6 col-lg-4").addClass("list");
-          $(".fables-product-img").removeClass("col-12").addClass("col-4"); 
-          $(".fables-product-block .card-body").removeClass("col-12").addClass("col-8"); 
+          $(".fables-product-img").removeClass("col-12").addClass("col-4");
+          $(".fables-product-block .card-body").removeClass("col-12").addClass("col-8");
       }
     else{
           $(".fables-product-block").addClass("col-md-6 col-lg-4").removeClass("list");
-          $(".fables-product-img").removeClass("col-4").addClass("col-12"); 
+          $(".fables-product-img").removeClass("col-4").addClass("col-12");
           $(".fables-product-block .card-body").removeClass("col-8").addClass("col-12");
     }
 });
-    
-    
+
+
 $('[data-countdown]').each(function() {
   var $this = $(this), finalDate = $(this).data('countdown');
   $this.countdown(finalDate, function(event) {
     $this.html(event.strftime('<div class="row"><div class="col-6 col-lg-3 mb-3 mb-lg-0 comming-soon-counter">%D <br> <span>Days</span></div>  <div class="col-6 col-lg-3 comming-soon-counter"> %H <br> <span>Hours</span> </div> <div class="col-6 col-lg-3 mb-3 mb-lg-0 comming-soon-counter">  %M <br> <span>Minutes</span></div> <div class="col-6 col-lg-3 mb-3 mb-lg-0 comming-soon-counter"> %S <br> <span>Seconds</span></div></div>'));
   });
 });
-    
-  
-    
+
+
+
 // sync carousel
 
   var sync1 = $("#sync1");
@@ -205,7 +205,7 @@ $('[data-countdown]').each(function() {
     })
     .owlCarousel({
     items : slidesPerPage,
-    dots: false, 
+    dots: false,
     nav: false,
     mouseDrag:true,
     smartSpeed: 200,
@@ -217,18 +217,18 @@ $('[data-countdown]').each(function() {
   function syncPosition(el) {
     //if you set loop to false, you have to restore this next line
     //var current = el.item.index;
-    
+
     //if you disable loop you have to comment this block
     var count = el.item.count-1;
     var current = Math.round(el.item.index - (el.item.count/2) - .5);
-    
+
     if(current < 0) {
       current = count;
     }
     if(current > count) {
       current = 0;
     }
-    
+
     //end block
 
     sync2
@@ -239,7 +239,7 @@ $('[data-countdown]').each(function() {
     var onscreen = sync2.find('.owl-item.active').length - 1;
     var start = sync2.find('.owl-item.active').first().index();
     var end = sync2.find('.owl-item.active').last().index();
-    
+
     if (current > end) {
       sync2.data('owl.carousel').to(current, 100, true);
     }
@@ -247,23 +247,23 @@ $('[data-countdown]').each(function() {
       sync2.data('owl.carousel').to(current - onscreen, 100, true);
     }
   }
-  
+
   function syncPosition2(el) {
     if(syncedSecondary) {
       var number = el.item.index;
       sync1.data('owl.carousel').to(number, 100, true);
     }
   }
-  
+
   sync2.on("click", ".owl-item", function(e){
     e.preventDefault();
     var number = $(this).index();
     sync1.data('owl.carousel').to(number, 300, true);
   });
-   
-    
 
-    
+
+
+
 $('#fables-testimonial-carousel').owlCarousel({
         loop:true,
         dots:true,
@@ -271,15 +271,15 @@ $('#fables-testimonial-carousel').owlCarousel({
         autoplay:true,
         items:1,
         navText: ['<span class="fables-iconarrow-left"></span>','<span class="fables-iconarrow-right"></span>']
-        
+
         });
-    
+
  $('#fables-partner-carousel').owlCarousel({
         loop:true,
         dots:false,
         autoplay:true,
         margin:20,
-        nav:true,     
+        nav:true,
         navText:['<i class="fa fa-chevron-left fables-main-text-color fables-main-border-color fables-partner-nav-icon" aria-hidden="true"></i>' , '<i class="fa fa-chevron-right fables-main-text-color fables-main-border-color fables-partner-nav-icon" aria-hidden="true"></i>'],
         responsive:{
             0:{
@@ -299,12 +299,12 @@ $('#fables-testimonial-carousel').owlCarousel({
             }
             }
         });
-    
- 
+
+
 
 
 // multi event gallery timeLine
- 
+
 $('.timeline').timelify({
 
   // animation types
@@ -317,11 +317,11 @@ $('.timeline').timelify({
 
   // trigger position in pixels
   offset: 150
-  
+
 });
 
 
-    
+
 
 });
 
@@ -355,7 +355,4 @@ $(function ($) {
            options = $.extend({}, options || {}, $this.data('countToOptions') || {});
            $this.countTo(options);
        }
-   } 
-
-
- 
+   }
